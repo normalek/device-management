7610dc5f00838dac7eb7879c11004608bc0bb363
dmitrykuznetsov <dmitry.k@bsfdv.com>
2024-03-03 23:15:41 +0400

    more logs

--- src/main/kotlin/com/fancycomp/devicebooking/service/DeviceManagementServiceImpl.kt
+++ src/main/kotlin/com/fancycomp/devicebooking/service/DeviceManagementServiceImpl.kt
@@ -10,6 +10,8 @@ import com.fancycomp.devicebooking.mapper.DeviceMapper
 import com.fancycomp.devicebooking.messaging.dto.DeviceEvent
 import com.fancycomp.devicebooking.messaging.MessageProducer
 import com.fancycomp.devicebooking.repository.DeviceRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -20,22 +22,35 @@ class DeviceManagementServiceImpl(
     private val deviceRepository: DeviceRepository,
     private val messageProducer: MessageProducer): DeviceManagementService {
 
+    companion object {
+        val LOGGER: Logger = LoggerFactory.getLogger(DeviceManagementServiceImpl::class.java)
+    }
+
     @Transactional
     override fun bookDeviceById(id: Int, bookedBy: String):DeviceDto {
-        val device = deviceRepository.findById(id).orElseThrow{ DeviceNotFoundException(id) }
+        val device = deviceRepository.findById(id).orElseThrow{
+            LOGGER.error("Device with id $id not found in the system for booking")
+            DeviceNotFoundException(id)
+        }
         processBooking(device, bookedBy)
         return DeviceMapper.toDeviceDto(device)
     }
 
     @Transactional
     override fun bookDeviceByDeviceType(id: Int, bookedBy: String):DeviceDto {
-        val device = deviceRepository.findAllByDeviceType_Id(id).ifEmpty { throw DeviceTypeNotFoundException(id) }.first()
+        val device = deviceRepository.findAllByDeviceType_Id(id).ifEmpty {
+            LOGGER.error("Device with id $id not found in the system for booking by device type")
+            throw DeviceTypeNotFoundException(id)
+        }.first()
         processBooking(device, bookedBy)
         return DeviceMapper.toDeviceDto(device)
     }
 
     private fun processBooking(device: Device, bookedBy: String) {
-        if (!device.isAvailable) throw DeviceAlreadyBookedException(device.id)
+        if (!device.isAvailable) {
+            LOGGER.error("Device with id ${device.id} is already booked hence cannot be released")
+            throw DeviceAlreadyBookedException(device.id)
+        }
         device.isAvailable = false
         device.bookedBy = bookedBy
         device.bookedDateTime = LocalDateTime.now()
@@ -45,8 +60,14 @@ class DeviceManagementServiceImpl(
 
     @Transactional
     override fun releaseDeviceById(id: Int):DeviceDto {
-        val device = deviceRepository.findById(id).orElseThrow{DeviceNotFoundException(id)}
-        if (device.isAvailable) throw DeviceAlreadyReleasedException(device.id)
+        val device = deviceRepository.findById(id).orElseThrow{
+            LOGGER.error("Device with id $id not found in the system for releasing")
+            DeviceNotFoundException(id)
+        }
+        if (device.isAvailable) {
+            LOGGER.error("Device with id $id is already released hence cannot be booked")
+            throw DeviceAlreadyReleasedException(device.id)
+        }
         device.isAvailable = true
         device.bookedBy = null
         device.releasedDateTime = LocalDateTime.now()
